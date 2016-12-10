38126974c1bbe956f42652b06825e3baa20518ec
上村 拓也 <m0114500@edu.teu.ac.jp>
2016-12-10 19:29:58 +0900

    playerHP&move

---
+++ Assets/Scripts/player_camera_script.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_camera_script : MonoBehaviour
+{
+    private float _rotationSpeed, _rotationCnt, _playerSpeed;
+    public float _life, _lifeMax;
+    //------------------------
+    int tf;
+    //------------------------
+    void Start()
+    {
+        _rotationCnt = 0;
+        _rotationSpeed = 2;
+        _playerSpeed = 0.1f;
+        //----------------------
+        _life = 10;
+        _lifeMax = _life;
+        tf = 1;
+        //----------------------
+    }
+
+
+    void Update()
+    {
+        PlayerMove();
+      //  Debug.Log(_life);
+    }
+    void PlayerMove()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, _rotationCnt, transform.rotation.z);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, _playerSpeed, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -_playerSpeed, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rotationCnt -= _rotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rotationCnt += _rotationSpeed;
+        }
+        //---------------------------------------
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (tf == 1)
+            {
+                tf = 0;
+                _life -= 1;
+            }
+        }
+        else
+        {
+            tf = 1;
+
+        }
+        //-----------------------------------------
+    }
+}
+    
---
+++ Assets/Scripts/player_life_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_life_script : MonoBehaviour {
+    public Image bar_image;
+    public GameObject player;
+    player_camera_script play;
+    
+   
+    // Use this for initialization
+    void Start () {
+        bar_image.fillAmount = 1;
+        play = player.GetComponent<player_camera_script>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       bar_image.fillAmount = (play._life / play._lifeMax);
+      //Debug.Log(_pLife);
+    }
+}
