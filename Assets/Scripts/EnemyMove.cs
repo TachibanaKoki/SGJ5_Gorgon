3ffe0d44742f78398022be4297c09431eb7920c7
Kouki <standchat0820@gmail.com>
2016-12-10 14:51:47 +0900

    プラグインの更新、敵の素体作成

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour {
+
+    NavMeshAgent m_Agent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+        m_Agent.SetDestination(Vector3.zero);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/EnemyShot.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Bullet;
+
+    [SerializeField]
+    float ShotRange = 5.0f;
+    [SerializeField]
+    float ShotTime = 0.5f;
+
+    [SerializeField]
+    GameObject Player;
+
+    bool isShot;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isShot = false;
+        StartCoroutine("ShotCoroutine");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Vector3.Distance(transform.position,Player.transform.position)<ShotRange)
+        {
+            isShot = true;
+        }
+        else
+        {
+            isShot = false;
+        }
+	}
+
+    IEnumerator ShotCoroutine()
+    {
+        while(true)
+        {
+            if(isShot)
+            {
+                Shot();
+            }
+
+            yield return new WaitForSeconds(ShotTime);
+        }
+    }
+
+    void Shot()
+    {
+        GameObject go = GameObject.Instantiate(Bullet,transform.position+transform.forward,Quaternion.identity);
+        Vector3 vec = Player.transform.position-transform.position;
+        go.GetComponent<Rigidbody>().AddForce(vec*0.5f);
+    }
+}
