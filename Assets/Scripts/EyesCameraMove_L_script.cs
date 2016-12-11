920237a15a8c8ace2bca1411c1a09e9a745dd41f
上村 拓也 <m0114500@edu.teu.ac.jp>
2016-12-11 13:00:32 +0900

    eyes_move

---
+++ Assets/Scripts/EyesCameraMove_L_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.EyeTracking;
+using Tobii.EyeX;
+
+
+public class EyesCameraMove_L_script : MonoBehaviour {
+    public GameObject camera_L;
+    GazeAware eyesL;
+    player_camera_script camel;
+    // Use this for initialization
+    void Start () {
+        eyesL = GetComponent<GazeAware>();
+        camel = camera_L.GetComponent<player_camera_script>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (eyesL.HasGazeFocus)
+        {
+            camel.LeftRot();
+        }
+    }
+}
---
+++ Assets/Scripts/EyesCameraMove_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.EyeTracking;
+using Tobii.EyeX;
+
+public class EyesCameraMove_script : MonoBehaviour {
+    public GameObject camera;
+    GazeAware eyesR;
+    player_camera_script came;
+
+    // Use this for initialization
+    void Start () {
+        eyesR = GetComponent<GazeAware>();
+        came = camera.GetComponent<player_camera_script>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (eyesR.HasGazeFocus)
+        {
+            came.RightRot();
+        }
+    }
+}
